2aa6824e921cab36e21ca4df1d4940b1fd6ebc5d
TKdevlop <34345518+TKdevlop@users.noreply.github.com>
2018-07-24 20:37:11 +0530

    cors

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const bodyPaser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain)
     // create reusable transporter object using the default SMTP transport
     const mailTransport = nodemailer.createTransport(
         `smtps://sendmail9411@gmail.com:mynameisjeff@smtp.gmail.com`);
